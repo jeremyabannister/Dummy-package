b6e741b4dc8945a0840d1f012cd08d1367be8fcf
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2024-07-28 14:53:11 +0200

    Migrated from the predecessor, `DummyModule`.

--- Package.swift
+++ Package.swift
@@ -1,23 +1,27 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
+///
 import PackageDescription
 
+
+///
 let package = Package(
     name: "Dummy-package",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "Dummy-package",
-            targets: ["Dummy-package"]),
+            name: "Dummy-module",
+            targets: ["Dummy-module"]
+        ),
     ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Dummy-package"),
+            name: "Dummy-module",
+            dependencies: []
+        ),
         .testTarget(
-            name: "Dummy-packageTests",
-            dependencies: ["Dummy-package"]),
+            name: "Dummy-module-tests",
+            dependencies: ["Dummy-module"]
+        ),
     ]
 )
---
+++ Sources/Dummy-module/Dummy.swift
@@ -0,0 +1,9 @@
+//
+//  Dummy.swift
+//
+//
+//  Created by Jeremy Bannister on 7/28/24.
+//
+
+///
+public enum Dummy { }
